d85e4dc1f55921f5e48da6add3825a8ff40029ca
Victor Mours <penayo.gustavo@gmail.com>
2022-10-31 01:14:30 -0300

    Add commits pagination

--- src/pages/commits.tsx
+++ src/pages/commits.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Table, Spinner, Card, Pagination } from 'flowbite-react';
+import React, { useState, useEffect } from 'react';
+import { Alert, Table, Spinner, Card, Pagination } from 'flowbite-react';
+import { HiInformationCircle } from 'react-icons/hi'
 import { useParams } from "react-router-dom";
 import { useGetCommits } from '../services/github-api';
 import { Commit } from '../models/Commit' ;
@@ -7,8 +8,13 @@ import { Commit } from '../models/Commit' ;
 function Commits () {
   const { branchName } = useParams<{ branchName: string}>() ;
   const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
   const [key, setKey] = useState(0);
-  const [loading, data, error] = useGetCommits(key, branchName, page, 15);
+  const [loading, data, error] = useGetCommits(key, branchName, page, perPage);
+
+  useEffect(() => {
+    setPage(1)
+  }, [branchName])
 
   function onPageChange (currentPage: number) {
     setPage(currentPage)
@@ -49,6 +55,16 @@ function Commits () {
       <div className="w-full  tracking-tight text-gray-900 dark:text-white">
         <h5 className="mb-4 text-2xl font-bold">Branch {branchName}'s Commit List</h5>
         { loading && <LoadingItem /> }
+        { page > 1 && (data as Commit[]).length === 0 &&
+          <Alert color="failure" icon={HiInformationCircle}>
+            <span>
+              <span className="font-medium">
+                Info!
+              </span>
+              There is no more data to show.
+            </span>
+          </Alert>
+        }
         { !loading && (data as Commit[]).length > 0 &&
           <div className="mt-4">
             <Table hoverable={true}>
@@ -65,17 +81,17 @@ function Commits () {
               </Table.Head>
               <TableItems />
             </Table>
-            <div  className="flex items-center justify-center text-center">
-              <Pagination
-                currentPage={page}
-                layout="navigation"
-                onPageChange={onPageChange}
-                showIcons={true}
-                totalPages={100}
-              />
-            </div>
           </div>
         }
+        <div  className="flex items-center justify-center text-center">
+          <Pagination
+            currentPage={page}
+            layout="navigation"
+            onPageChange={onPageChange}
+            showIcons={true}
+            totalPages={100}
+          />
+        </div>
       </div>
     </>
   )
